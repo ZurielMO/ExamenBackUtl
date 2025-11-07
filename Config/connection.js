2a9c05947e0f1336c6d17abbebd67cccc4de1d9e
IDGS-803-22001034 <80085@alumnos.utleon.edu.mx>
2025-11-07 12:39:15 -0600

    cambios de correcciones

--- Config/connection.js
+++ Config/connection.js
@@ -2,16 +2,14 @@ import dotenv from "dotenv";
 dotenv.config();
 import { Sequelize } from "sequelize";
 
-
 const databaseConnection = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false
-    }
+    },
   },
-  logging: false
+  logging: false,
 });
 
 export default databaseConnection;
